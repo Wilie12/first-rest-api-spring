be4c52c246b7d6c1a1fe2168583b773003d88a12
Wilie12 <albertw536@gmail.com>
2024-12-18 16:36:52 +0100

    Task 2.B completed.

--- src/main/java/com/nn/first_rest_api_spring/product/api/ProductController.java
+++ src/main/java/com/nn/first_rest_api_spring/product/api/ProductController.java
@@ -3,6 +3,7 @@ package com.nn.first_rest_api_spring.product.api;
 import com.nn.first_rest_api_spring.product.api.request.ProductRequest;
 import com.nn.first_rest_api_spring.product.api.response.ProductResponse;
 import com.nn.first_rest_api_spring.product.service.ProductService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,8 +17,16 @@ public class ProductController {
     public ProductController(ProductService productService) { this.productService = productService; }
 
     @PostMapping
+    @Operation(summary = "Create product")
     public ResponseEntity<ProductResponse> create(@RequestBody ProductRequest productRequest) {
         ProductResponse productResponse = productService.create(productRequest);
         return new ResponseEntity<>(productResponse, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    @Operation(summary = "Find product by id")
+    public ResponseEntity<ProductResponse> find(@PathVariable Long id) {
+        ProductResponse productResponse = productService.find(id);
+        return ResponseEntity.status(HttpStatus.OK).body(productResponse);
     }
 }
--- src/main/java/com/nn/first_rest_api_spring/product/repository/ProductRepository.java
+++ src/main/java/com/nn/first_rest_api_spring/product/repository/ProductRepository.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Repository;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 @Repository
 public class ProductRepository {
@@ -22,5 +23,9 @@ public class ProductRepository {
         map.put(counter, entity);
         counter++;
         return entity;
+    }
+
+    public Optional<Product> findById(Long id) {
+        return Optional.ofNullable(map.get(id));
     }
 }
